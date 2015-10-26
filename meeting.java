dee4952cf66b8442f246d4771538e87ee856086e
akhilmsreenath <akhilmsreenath@gmail.com>
2015-10-26 00:15:55 -0700

    Create meeting.java

---
+++ meeting.java
@@ -0,0 +1,12 @@
+import Employee.java;
+import Room.java;
+public class meeting {
+private int meetingId;
+private Set<Employee> employees;
+private Room room;
+Date timeSlot ;
+
+//getter setter methods
+
+
+}
